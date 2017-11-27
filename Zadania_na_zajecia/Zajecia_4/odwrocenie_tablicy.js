13427dc931136eb9fad84d729c9dbc231bc62617
xard43 <s426243@PC14I>
2017-11-27 16:53:32 +0100

    tablica

---
+++ Zadania_na_zajecia/Zajecia_4/odwrocenie_tablicy.js
@@ -0,0 +1,11 @@
+function reverse(tab){
+  for(var i=tab.length-1; i>=0;i--)
+  {
+    console.log(tab[i]);
+  }
+
+}
+
+
+var numbers = [11, 6, 12, 1, 3, 14, 10, 0, 13, 4, 15, 5, 9, 17, 7, 16, 2, 8];
+reverse(numbers);
